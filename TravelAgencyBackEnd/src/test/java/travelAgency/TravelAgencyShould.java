e8cd4ce52982e92c2f6647a34e2cc5c6a80f4155
amir-original <amirrahmani7017@gmail.com>
2023-03-02 10:23:05 +0330

    init project

---
+++ TravelAgencyBackEnd/src/test/java/travelAgency/TravelAgencyShould.java
@@ -0,0 +1,13 @@
+package travelAgency;
+
+import org.junit.jupiter.api.Test;
+
+public class TravelAgencyShould {
+
+    @Test
+    void book_a_flight() {
+        TravelAgency app = new TravelAgency();
+        // book a flight
+
+    }
+}
